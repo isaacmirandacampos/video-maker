276ca2193790413c14ccda014f4aecec95bcb67d
isaacmirandacampos <isaacdmcampos@gmail.com>
2021-03-04 16:49:10 -0300

    feat: download images

--- scripts/image.js
+++ scripts/image.js
@@ -1,14 +1,15 @@
 const google = require("googleapis").google;
+const imageDownloader = require("image-downloader");
 const state = require("./state");
 const googleSearchCredentials = require("../credentials/google-search.json");
-
 const customSearch = google.customsearch("v1");
 const image = async () => {
   let content = state.load();
 
-  content = await fetchImagesOfAllSentences(content);
-
-  state.save(content);
+  // content = await fetchImagesOfAllSentences(content);
+  await downloadAllImages(content);
+  // tate.save(content);
+  process.exit(0);
 };
 
 const fetchImagesOfAllSentences = async (content) => {
@@ -32,6 +33,48 @@ const fetchGoogleAndReturnImagesLinks = async (query) => {
   });
 
   return response.data.items.map((item) => item.link);
+};
+
+const downloadAllImages = async (content) => {
+  content.downloadedImages = [];
+
+  for (
+    let sentenceIndex = 0;
+    sentenceIndex < content.sentences.length;
+    sentenceIndex++
+  ) {
+    const images = content.sentences[sentenceIndex].images;
+
+    for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+      const imageUrl = images[imageIndex];
+      try {
+        if (content.downloadedImages.includes(imageUrl)) {
+          throw new Error("Imagem já foi baixada");
+        }
+
+        await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+
+        content.downloadedImages.push(imageUrl);
+
+        console.log(
+          `> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`
+        );
+        break;
+      } catch (error) {
+        console.log(
+          `> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}) ${error}`
+        );
+      }
+    }
+  }
+};
+
+const downloadAndSave = (url, fileName) => {
+  return imageDownloader.image({
+    url,
+    url,
+    dest: `./content/${fileName}`,
+  });
 };
 
 module.exports = image;
